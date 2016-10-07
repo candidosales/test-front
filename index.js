7b124dea3f8e46febdd51e5d3ac9e113a5ae7ec8
candidosales <candidosg@gmail.com>
2016-10-07 01:16:46 -0300

    finalized

--- index.js
+++ index.js
@@ -1,45 +1,90 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     $('.gallery').slick({
         slidesToShow: 3,
         slidesToScroll: 3,
         dots: true,
-        infinite: false
+        infinite: false,
+        arrows: false
     });
 
-  $filterItems =  $('.filter-item');
+    $filterItems = $('.filter-item');
+    $filterTypeItems = $('.filter-type-item');
 
-  $itemsOriginal = $('.item');
-  $itemsCache = $itemsOriginal.clone();
+    var typeFilter = "1";
 
-  console.log($itemsCache.length);
+    $filterTypeItems.on('change', function () {
+        console.log($(this).val());
 
-  $filterItems.on('click', function() {
-    var checkeds = [];
-    var checkedsNot = [];
+        typeFilter = $(this).val();
+        filter();
+    });
 
-    $('input[type="checkbox"]').each(function() {
-        if($(this).is(':checked')) {
-            checkeds.push('.'+ $(this).attr('data-filter'));
-        } else {
-            checkedsNot.push('.'+ $(this).attr('data-filter'));
+    $filterItems.on('click', function () {
+       filter();
+    });
+
+    function filter() {
+         console.log("filter type", typeFilter);
+
+         var checkeds = [],
+            checkedsNot = [];
+
+        $('input[type="checkbox"]').each(function () {
+            if ($(this).is(':checked')) {
+                checkeds.push('.' + $(this).attr('data-filter'));
+            } else {
+                checkedsNot.push('.' + $(this).attr('data-filter'));
+            }
+        });
+
+        console.log('checkeds.length', checkeds.length);
+
+        if (checkeds.length > 0) {
+            if (checkeds.length > 1) {
+                console.log('tem agrupamento');
+                switch (typeFilter) {
+                    case "1":
+                        checkeds = checkeds.concat(groupCheckeds(checkeds));
+                        break;
+                    case "2":
+                        checkeds = groupCheckeds(checkeds);
+                        break;
+                }
+            }
+
+            console.log('checkeds final', checkeds);
+
+            checkeds = checkeds.join(', ').replace(/, $/, "");
+            checkedsNot = checkedsNot.join(', ').replace(/, $/, "");
+            console.log(checkeds, checkedsNot);
+
+            if (checkeds === "") {
+                checkeds = "*";
+                checkedsNot = "";
+            }
+
+            var allFilters = checkeds + ":not(" + checkedsNot + ")";
+            console.log(allFilters);
+
+            $('.filtering').slick('slickUnfilter').slick('slickFilter', allFilters);
         }
-    });
-        
-    checkeds = checkeds.join(', ').replace(/, $/, "");
-    checkedsNot = checkedsNot.join(', ').replace(/, $/, "");
-    console.log(checkeds, checkedsNot);
-
-    if(checkeds === "") {
-        checkeds = "*";
-        checkedsNot = "";
     }
 
-    var allFilters = checkeds + ":not(" + checkedsNot + ")";
-    console.log(allFilters);
+    function groupCheckeds(checkeds) {
+        var total = checkeds.length;
+        var groups = [];
 
-    $('.filtering').slick('slickUnfilter').slick('slickFilter' , allFilters);
-
-  }); 
+        for (var i = 0; i < total; i++) {
+            var element = checkeds[i];
+            console.log('element', element);
+            for (var y = i + 1; y < total; y++) {
+                groups.push(element.trim() + checkeds[y].trim());
+                console.log('- possibilite', element.trim() + checkeds[y].trim());
+            }
+        }
+        console.log('groups', groups);
+        return groups;
+    }
 
 });
